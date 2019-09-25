b2b1bcea7b0ce1e7c64c2a21b000eadade2bd124
Sarbjyotsngh <chahalsarbjyotsingh@gmail.com>
2019-09-25 08:13:32 +0530

    creating stateful class

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,33 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 4;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Right Button Pressed');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),
